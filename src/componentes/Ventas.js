28dcb2287b3de1a494c71b16d8d12286434bd130
Wendy Vallejo <wbvalpat24@gmail.com>
2023-12-10 12:06:41 -0600

    Corrección Ventas.js

--- src/componentes/Ventas.js
+++ src/componentes/Ventas.js
@@ -6,42 +6,26 @@ import MenuHamburguesa from './MenuHamburguesa';
 
 const Ventas = () => {
     const [productos, setProductos] = useState([]);
-    const [nuevoProducto, setNuevoProducto] = useState({
-        cantidad: 0,
-        producto: "",
-        precio: ""
-    });
-
-
-    const calcularSubtotal = (cantidad, precio) => cantidad * precio;
-
-    const calcularTotal = () => {
-        return productos.reduce((total, producto) => {
-            const precioNumerico = parseFloat(producto.precio);
-            return total + (isNaN(precioNumerico) ? 0 : calcularSubtotal(producto.cantidad, precioNumerico));
-        }, 0);
-    };
-
-
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setNuevoProducto((prevProducto) => ({
-            ...prevProducto,
-            [name]: name === "precio" ? value : value
-        }));
-    };
+    const [total, setTotal] = useState(0);
+    const [cantidad, setCantidad] = useState("");
+    const [producto, setProducto] = useState("");
+    const [precioUnitario, setPrecioUnitario] = useState("");
 
     const agregarProducto = () => {
-        if (nuevoProducto.cantidad && nuevoProducto.producto && nuevoProducto.precio) {
-            setProductos((prevProductos) => [...prevProductos, { ...nuevoProducto }]);
-            setNuevoProducto({
-                cantidad: "",
-                producto: "",
-                precio: ""
-            });
-        }
-    };
+        const cantidadValue = parseInt(cantidad, 10);
+        const precioUnitarioValue = parseFloat(precioUnitario);
 
+        if (isNaN(cantidadValue) || isNaN(precioUnitarioValue)) {
+            // Manejar el caso en que los valores no sean números válidos
+            return;
+        }
+
+        const subtotal = cantidadValue * precioUnitarioValue;
+
+        setProductos([...productos, { producto, cantidad: cantidadValue, precioUnitario: precioUnitarioValue, subtotal }]);
+        setTotal(total + subtotal);
+    };
+    
     return (
         <div className="registro">
             <MenuHamburguesa />
@@ -50,51 +34,54 @@ const Ventas = () => {
                 <input
                     className="cantidad"
                     placeholder="Cantidad"
-                    value={nuevoProducto.cantidad}
-                    onChange={handleInputChange}
+                    value={cantidad}
+                    onChange={(e) => setCantidad(e.target.value)}
                 />
                 <input
                     className="producto"
                     placeholder="Producto"
-                    value={nuevoProducto.producto}
-                    onChange={handleInputChange}
+                    value={producto}
+                    onChange={(e) => setProducto(e.target.value)}
                 /> <br />
                 <input
                     className="precio"
                     name="precio"
                     placeholder="Precio Unitario"
-                    value={nuevoProducto.precio}
-                    onChange={handleInputChange}
+                    value={precioUnitario}
+                    onChange={(e) => setPrecioUnitario(e.target.value)}
                 />
-                <button className="agregar" onClick={agregarProducto}>Agregar</button>
-            </div>
-            <div className="scroll-panel">
-                <table>
-                    <thead className="ventas">
-                        <tr className="ventas">
-                            <th className="ventas">Cantidad</th>
-                            <th className="ventas">Producto</th>
-                            <th className="ventas">Precio Unitario</th>
-                            <th className="ventas">Subtotal</th>
-                        </tr>
-                    </thead>
-                    <tbody className="ventas">
-                        {productos.map((producto, index) => (
-                            <tr key={index} className="ventas">
-                                <td className="ventas">{producto.cantidad}</td>
-                                <td className="ventas">{producto.producto}</td>
-                                <td className="ventas">${producto.precio}</td>
-                                <td className="ventas">${calcularSubtotal(producto.cantidad, producto.precio)}</td>
+                <button className="agregar" onClick={agregarProducto}>
+                    Agregar
+                </button>
+                <div className="scroll-panel">
+                    <table>
+                        <thead className="ventas">
+                            <tr className="ventas">
+                                <th className="ventas">Cantidad</th>
+                                <th className="ventas">Producto</th>
+                                <th className="ventas">Precio Unitario</th>
+                                <th className="ventas">Subtotal</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody className="ventas">
+                            {productos.map((producto, index) => (
+                                <tr key={index} className="ventas">
+                                    <td className="ventas">{producto.cantidad}</td>
+                                    <td className="ventas">{producto.producto}</td>
+                                    <td className="ventas">${producto.precioUnitario}</td>
+                                    <td className="ventas">${producto.subtotal}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+                <h3 className="total">Total: ${total}</h3>
+                <div className="btns">
+                    <button className="btn-finalizar">Finalizar Venta</button>
+                    <button className="btn-cancelar">Cancelar Venta</button>
+                </div>
             </div>
-            <h3 className="total">Total: ${calcularTotal()}</h3>
-            <div className="btns">
-                <button className="btn-finalizar">Finalizar Venta</button>
-                <button className="btn-cancelar">Cancelar Venta</button>
-            </div>
+
         </div>
     );
 }
