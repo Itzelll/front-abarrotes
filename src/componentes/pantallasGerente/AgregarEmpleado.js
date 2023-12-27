3669ecbd08cc5649abcd61088f19f3f0b2cd5617
ZurisaddaiRJ <zuleyma.juarez_2002@hotmail.com>
2023-12-27 13:01:48 -0600

    Edit login, AgregarEmpl,registroEmpl,EliminarEmpl

--- src/componentes/pantallasGerente/AgregarEmpleado.js
+++ src/componentes/pantallasGerente/AgregarEmpleado.js
@@ -1,33 +1,92 @@
 import './style/AgregarEmpleado.css';
 import MenuHamburguesa from '../MenuHamburguesa';
+import React, { useState } from 'react';
 
 const AgregarEmpleado = () => {
+    const [empleado, setEmpleado] = useState({
+        nombre: '',
+        apellidos: '',
+        contrasenia: '',
+        idRol: ''
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setEmpleado({ ...empleado, [name]: value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('http://localhost:8080/api/empleados', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(empleado)
+            });
+
+            if (response.ok) {
+                console.log('Empleado agregado correctamente');
+                // Puedes redirigir o hacer otras acciones después de agregar el empleado
+            } else {
+                console.error('Error al agregar el empleado');
+            }
+        } catch (error) {
+            console.error('Error de red:', error);
+        }
+    };
     return (
         <div className="contenedor">
             <MenuHamburguesa />
             <h1>Agregar Empleado</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     Nombre:
-                    <input type="text" className="datos" />
+                    <input
+                        type="text"
+                        className="datos"
+                        name="nombre"
+                        value={empleado.nombre}
+                        onChange={handleInputChange}
+                    />
                 </label>
                 <br />
                 <label>
                     Apellidos:
-                    <input type="text" className="datos" />
+                    <input
+                        type="text"
+                        className="datos"
+                        name="apellidos"
+                        value={empleado.apellidos}
+                        onChange={handleInputChange}
+                    />
                 </label>
                 <br />
                 <label>
                     Contraseña
-                    <input type="password" className="datos" />
+                    <input
+                        type="password"
+                        className="datos"
+                        name="contrasenia"
+                        value={empleado.contrasenia}
+                        onChange={handleInputChange}
+                    />
                 </label>
                 <br />
                 <label>
                     ID Rol:
-                    <input type="text" className="datos" />
+                    <input
+                        type="text"
+                        className="datos"
+                        name="idRol"
+                        value={empleado.idRol}
+                        onChange={handleInputChange}
+                    />
                 </label>
                 <br />
-                <button className='confirmar'>Confirmar</button>
+                <button type='submit' className='confirmar'>Confirmar</button>
             </form>
         </div>
     );
--- src/componentes/pantallasGerente/registroEmp.js
+++ src/componentes/pantallasGerente/registroEmp.js
@@ -2,15 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 import './style/registroEmp.css';
 import MenuHamburguesa from '../MenuHamburguesa';
+import { useState, useEffect } from "react";
 
 const RegistroEmp = () => {
+    const [empleados, setEmpleados] = useState([]);
+
+    useEffect(() => {
+        // Realizar la solicitud GET al backend cuando el componente se monta
+        fetch("http://localhost:8080/api/empleados")
+            .then(response => response.json())
+            .then(data => {
+                // Actualizar el estado con los datos del backend
+                setEmpleados(data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }, []); // La dependencia vacía asegura que useEffect se ejecute solo una vez al montar el componente
+
     return (
         <div className="registro">
             <MenuHamburguesa />
             <h1>Registro de empleados</h1>
             <div className="botones">
-            <Link to="/agregarEmpleado"><button className="btn-crud">Agregar Empleado</button></Link>
-            <Link to="/eliminarEmpleado"><button className="btn-crud-1">Eliminar Empleado</button></Link>
+                <Link to="/agregarEmpleado"><button className="btn-crud">Agregar Empleado</button></Link>
+                <Link to="/eliminarEmpleado"><button className="btn-crud-1">Eliminar Empleado</button></Link>
             </div>
             <table className="registrosEmp">
                 <thead>
@@ -22,27 +38,16 @@ const RegistroEmp = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Jose</td>
-                        <td>Sanchez</td>
-                        <td>1</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>María</td>
-                        <td>Jimenez</td>
-                        <td>2</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Carlos</td>
-                        <td>Castro</td>
-                        <td>1</td>
-                    </tr>
+                    {empleados.map(empleado => (
+                        <tr key={empleado.idEmpleado}>
+                            <td>{empleado.idEmpleado}</td>
+                            <td>{empleado.nombre}</td>
+                            <td>{empleado.apellidos}</td>
+                            <td>{empleado.idRol}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
-            
         </div>
     );
 };
