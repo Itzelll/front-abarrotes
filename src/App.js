5e717896d525a61dd5a86a4d3892ce1a2adc11d0
ZurisaddaiRJ <zuleyma.juarez_2002@hotmail.com>
2024-01-02 16:03:46 -0600

    Delete componentes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginForm from './componentes/login';
-import SalesReportAbarrotes from './componentes/pantallasGerente/reportes/salesReportAbarrotes';
 import RegistroEmp from './componentes/pantallasGerente/registroEmp';
 import InfoVentas from './componentes/pantallasGerente/infoVentas';
-import CrearNota from './componentes/pantallasGerente/crearNotaGastos';
 import AgregarEmpleado from './componentes/pantallasGerente/AgregarEmpleado';
 import EliminarEmpleado from './componentes/pantallasGerente/EliminarEmpleado';
-import SalesReportBanqueteria from './componentes/pantallasGerente/reportes/salesReportBanqueteria';
 import Ventas from './componentes/Ventas'
-import SalesReportFerreteria from './componentes/pantallasGerente/reportes/salesReportFerreteria';
-import NotaNuevaGastos from './componentes/pantallasGerente/notaNuevaGastos';
-import VentasMensualesAbarrotes from './componentes/pantallasGerente/VentasMensualesAbarrotes';
-import VentasMensualesBanqueteria from './componentes/pantallasGerente/VentasMensualesBanqueteria';
-import VentasMensualesFerreteria from './componentes/pantallasGerente/VentasMensualesFerreteria';
 import Catalogo from './componentes/pantallasGerente/productos/catalogo';
 import CreateProduct from './componentes/pantallasGerente/productos/agregar';
 import UpdateProduct from './componentes/pantallasGerente/productos/modificar';
@@ -39,19 +31,11 @@ function App() {
       
       <Routes>
         <Route path="/" element={<LoginForm />} />
-        <Route path="/salesReportAbarrotes" element={<SalesReportAbarrotes />} />
         <Route path="/registroEmpleado" element={<RegistroEmp />} />
         <Route path="infoVentas" element={<InfoVentas />} />
-        <Route path="/crearNotaGastos" element={<CrearNota />} />
         <Route path="/agregarEmpleado" element={<AgregarEmpleado />} />
         <Route path="/eliminarEmpleado" element={<EliminarEmpleado />} />
-        <Route path="/salesReportBanqueteria" element={<SalesReportBanqueteria />} />
         <Route path="/ventas" element={<Ventas />} />
-        <Route path="/salesReportFerreteria" element={<SalesReportFerreteria />} />
-        <Route path='/notaNuevaGastos' element={<NotaNuevaGastos />} />
-        <Route path='/ventasMensualesAbarrotes' element={<VentasMensualesAbarrotes />}></Route>
-        <Route path='/ventasMensualesBanqueteria' element={<VentasMensualesBanqueteria />}></Route>
-        <Route path='/ventasMensualesFerreteria' element={<VentasMensualesFerreteria />}></Route>
         <Route path="/catalogo" element={<Catalogo />} />
         <Route path="/agregarProducto" element={<CreateProduct />} />
         <Route path="/modificarProducto" element={<UpdateProduct />} />
